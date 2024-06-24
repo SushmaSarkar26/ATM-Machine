d50ac9232c98c94e5b46d8e713fd0c8206e2aa4c
Sushma Sarkar <123677590+SushmaSarkar26@users.noreply.github.com>
2024-06-24 20:11:54 +0530

    Update ATM Machine.py

--- ATM Machine.py
+++ ATM Machine.py
@@ -4,7 +4,7 @@ import time
 print("Please insert Your CARD")
 
 #for card processing
-time.sleep(5)
+time.sleep(1)
 
 password = 1234
 
@@ -16,11 +16,11 @@ balance = 5000
 
 #checking pin is valid or not 
 if pin == password:
+	
     #loop will run user get free 
     while True:
-
+	    
         #Showing  info to user
-
         print(""" 
 			1 == balance
 			2 == withdraw balance
@@ -34,47 +34,26 @@ if pin == password:
             option = int(input("Please enter your choise "))
         except:
             print("Please enter valid option")
-        
-        #for option 1        
+                
         if option == 1:
             print(f"Your current balance is {balance}")
                                      
         if option == 2:
-
             withdraw_amount = int(input("please enter withdraw_amount "))
-
-            
-
             balance = balance - withdraw_amount
-
             print(f"{withdraw_amount} is debited from your account")
-
-            
-
-            print(f"your updated balance is {balance}")
-
-            
+            print(f"your updated balance is {balance}")        
 
         if option == 3:
-
-            deposit_amount = int(input("please enter deposit_amount"))
-
-            balance = balance + deposit_amount
-
-            
-
+            deposit_amount = int(input("please enter deposit_amount "))
+            balance = balance + deposit_amount        
             print(f"{deposit_amount} is credited to your account")
-
-
-
             print(f"your updated balance is {balance}")
 
-
-
         if option == 4:
-
             break
 
 
 else:
     print("wrong pin Please try again")
+	
